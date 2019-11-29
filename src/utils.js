4a79d17665c776c33c472158a9b2d631945d8664
moa1520 <douit_be@naver.com>
2019-11-30 01:09:37 +0900

    sendMail function created

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,32 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 import { adjectives, nouns } from "./words";
+import nodemailer from "nodemailer";
+import mg from "nodemailer-mailgun-transport";
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+const sendMail = email => {
+  const auth = {
+    auth: {
+      api_key: process.env.API_KEY,
+      domain: process.env.DOMAIN
+    }
+  };
+  const client = nodemailer.createTransport(mg(auth));
+  return client.sendMail(email);
+};
+
+export const sendScretMail = (address, secret) => {
+  const email = {
+    from: "kozza@kostagram.com",
+    to: address,
+    subject: "🔒 Login Scret for Kostagram 🔒",
+    html: `Hello! Your login secret is ${secret}. <br />Copy paste on the App/Website to log in`
+  };
+  return sendMail(email);
 };
