a387e1e20ba1e0a276e4452cbcc696cd4bf85d22
moa1520 <douit_be@naver.com>
2019-12-01 17:38:56 +0900

    isFollowing, isSelf

---
+++ src/api/User/computed.js
@@ -0,0 +1,26 @@
+import { prisma } from "../../../generated/prisma-client";
+
+export default {
+  User: {
+    fullName: parent => {
+      return `${parent.firstName} ${parent.lastName}`;
+    },
+    isFollowing: (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      try {
+        return prisma.$exists.user({
+          AND: [{ id: user.id }, { following_some: { id: parentId } }]
+        });
+      } catch (e) {
+        console.log(e);
+        return false;
+      }
+    },
+    isSelf: (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      return user.id === parentId;
+    }
+  }
+};
