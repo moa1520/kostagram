a81b142a1e85b1c6e75a2757dd7bb0517bf130bf
moa1520 <douit_be@naver.com>
2019-12-05 20:05:32 +0900

    createAccount, requestSecret modified

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,6 +4,10 @@ export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { username, email, firstName = "", lastName = "", bio = "" } = args;
+      const exists = await prisma.$exists.user({ username });
+      if (exists) {
+        throw Error("아이디가 이미 사용 중입니다");
+      }
       await prisma.createUser({
         username,
         email,
