b2e0281fcccc8c636caf7c7b4ff3f454cb44727e
moa1520 <douit_be@naver.com>
2020-02-02 19:59:12 +0900

    Uploading to S3

--- src/upload.js
+++ src/upload.js
@@ -1,17 +1,31 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: "ap-northeast-2"
+});
 
 const upload = multer({
-  dest: "uploads/",
-  limits: {
-    fieldSize: 25 * 1024 * 1024
-  }
+  storage: multerS3({
+    s3: s3,
+    bucket: "kostagram",
+    metadata: function(req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function(req, file, cb) {
+      cb(null, Date.now().toString());
+    }
+  })
 });
 
 export const uploadMiddleware = upload.single("file");
 
 export const uploadController = (req, res) => {
   const {
-    file: { path }
+    file: { location }
   } = req;
-  res.json({ path });
+  res.json({ location });
 };
