977bc422757e801a4b44b51e6534248188b2bdd0
moa1520 <douit_be@naver.com>
2020-02-12 04:35:05 +0900

    Words change because of confirm to apple

--- src/upload.js
+++ src/upload.js
@@ -10,7 +10,7 @@ const s3 = new aws.S3({
 
 const upload = multer({
   storage: multerS3({
-    s3: s3,
+    s3,
     acl: "public-read",
     bucket: "kostagram",
     metadata: function(req, file, cb) {
